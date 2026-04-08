c02dd6a8295a3fb2dba8dcd750a9d02437e618ec
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-04-08 18:53:32 -0300

    feat: pkg config for read .env implemented

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type Config struct {
 	DB_HOST     string
 	DB_PORT     string
@@ -10,5 +17,17 @@ type Config struct {
 }
 
 func NewConfig() *Config {
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Println("erro ao ler a .env: ", err)
+		return nil
+	}
 
+	return &Config{
+		DB_HOST:     os.Getenv("DB_HOST"),
+		DB_PORT:     os.Getenv("DB_PORT"),
+		DB_USER:     os.Getenv("DB_USER"),
+		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
+		DB_NAME:     os.Getenv("DB_NAME"),
+		DB_SSLMODE:  os.Getenv("DB_SSLMODE"),
+	}
 }
